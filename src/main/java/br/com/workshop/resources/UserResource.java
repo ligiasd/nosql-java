0831feaef304064305512d827ebecce5e19d3c70
Ligiane <ligiane.alves.souza@gmail.com>
2022-05-11 18:36:11 -0300

    add class UserDTO

--- src/main/java/br/com/workshop/resources/UserResource.java
+++ src/main/java/br/com/workshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.workshop.resources;
 
 import br.com.workshop.domain.User;
+import br.com.workshop.dto.UserDTO;
 import br.com.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -17,10 +19,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
-
-
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
