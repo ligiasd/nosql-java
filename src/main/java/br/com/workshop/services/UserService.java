6efc14dd4cfa42f419edb374a240d33418178e58
Ligiane <ligiane.alves.souza@gmail.com>
2022-05-12 18:17:56 -0300

    add endpoint get ID User, class exception

--- src/main/java/br/com/workshop/services/UserService.java
+++ src/main/java/br/com/workshop/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.workshop.services;
 
 import br.com.workshop.domain.User;
 import br.com.workshop.repository.UserRepository;
+import br.com.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,6 +15,12 @@ public class UserService {
     private UserRepository repo;
 
     public List<User> findAll() {
+
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
