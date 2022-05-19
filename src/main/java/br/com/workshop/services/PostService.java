e285b22be6c96028bfca257760099ea3ea8fc6d3
Ligiane <ligiane.alves.souza@gmail.com>
2022-05-19 16:59:55 -0300

    add endpoint findPosts Id

---
+++ src/main/java/br/com/workshop/services/PostService.java
@@ -0,0 +1,20 @@
+package br.com.workshop.services;
+
+import br.com.workshop.domain.Post;
+import br.com.workshop.repository.PostRepository;
+import br.com.workshop.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
