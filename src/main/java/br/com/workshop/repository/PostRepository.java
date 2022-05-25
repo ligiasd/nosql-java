b872ff4d9371d0de8b0df4097edab00ffeb10278
Ligiane <ligiane.alves.souza@gmail.com>
2022-05-25 17:20:12 -0300

    add endpoint findByTitle

--- src/main/java/br/com/workshop/repository/PostRepository.java
+++ src/main/java/br/com/workshop/repository/PostRepository.java
@@ -3,5 +3,9 @@ package br.com.workshop.repository;
 import br.com.workshop.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
